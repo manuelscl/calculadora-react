36a25d638a9f7962d826c5dc3c689f7ee4aa62a3
manuelscl <manuelscardenaslopez@gmail.com>
2023-11-02 20:23:04 -0600

    Creando una calculadora con react

---
+++ src/componenetes/Logo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import freeCodeCampLogo from '../imagenes/freecodecamp-logo.png';
+
+const Logo = () => {
+    return (
+        <div className='freecodecamp-logo-contenedor'>
+            <img 
+                src={freeCodeCampLogo}
+                className='freecodecamp-logo'
+                alt='Logo de freeCodeCamp'
+            />
+        </div>
+    );
+};
+
+export default Logo;
